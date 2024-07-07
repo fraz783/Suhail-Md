448a121c1842f26a7e35d55f85bef6bfd8655b21
fraz783 <maliksarfraz61468@gmail.com>
2024-07-07 17:40:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://gi
 global.caption = process.env.CAPTION || global.caption || "MALIK MEHTAB " 
 
 
-global.devs = "923184474176" // Developer Contact
+global.devs = "923263429027" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923065120485";
 
